9d5416a2403b1d369e871cc6a1517d6570e27cc3
nandre <nicolas.andre@intech.lu>
2015-02-13 16:09:21 +0100

    crash

--- history/history.js
+++ history/history.js
@@ -5,17 +5,20 @@ gootAlias(document).ready(function(){
 		addCurrentTabToFav();
 	});
 	
+
+	
 	getUserCommentsFromUrl();
 	
 	getFriendsCommentsFromUrl();
+
 	
 });
+
 
 chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
     console.log("tab url : " + tabs[0].url);
     currentTabUrl = tabs[0].url;
 });
-
 
 function getCurrentTabUrl(){
 	return currentTabUrl;
@@ -43,24 +46,33 @@ function historyCallback(data){
 
 
 function getUserCommentsFromUrl(){
-	var currentTabUrl= getCurrentTabUrl();
-	var JSESSIONID = localStorage.JSESSIONID;
-	
-	var url = "http://goot.outsidethecircle.eu/plugin/comment/getFromUrl";
-	var data = { JSESSIONID : JSESSIONID, tabUrl : currentTabUrl};
-	
-	gootAjaxCall(url, data, updateCommentsCallbackForUser);
+	console.log("getUserCommentsFromUrl");
+	if(currentTabUrl != null){
+		var currentTabUrl= getCurrentTabUrl();
+		var JSESSIONID = localStorage.JSESSIONID;
+		
+		var url = "http://goot.outsidethecircle.eu/plugin/comment/getFromUrl";
+		var data = { JSESSIONID : JSESSIONID, tabUrl : currentTabUrl};
+		
+		gootAjaxCall(url, data, updateCommentsCallbackForUser);
+	} else {
+		setTimeout(getUserCommentsFromUrl(), 200);
+	}
 }
 
 
 function getFriendsCommentsFromUrl(){
-	var currentTabUrl= getCurrentTabUrl();
-	var JSESSIONID = localStorage.JSESSIONID;
-	
-	var url = "http://goot.outsidethecircle.eu/plugin/comment/getFromUrlAndFriends";
-	var data = { JSESSIONID : JSESSIONID, tabUrl : currentTabUrl};
-	
-	gootAjaxCall(url, data, updateCommentsCallbackForFriends);
+	if(currentTabUrl != null){
+		var currentTabUrl= getCurrentTabUrl();
+		var JSESSIONID = localStorage.JSESSIONID;
+		
+		var url = "http://goot.outsidethecircle.eu/plugin/comment/getFromUrlAndFriends";
+		var data = { JSESSIONID : JSESSIONID, tabUrl : currentTabUrl};
+		
+		gootAjaxCall(url, data, updateCommentsCallbackForFriends);
+	} else {
+		setTimeout(getFriendsCommentsFromUrl(), 200);
+	}
 }
 
 
@@ -74,18 +86,26 @@ function updateCommentsCallbackForUser(data){
 }
 
 function updateCommentsCallback(data, scope){
-	var result = gootAlias.parseJSON(data);
+    console.log("callback data for comments :", JSON.stringify(data));
+
+	var result = gootAlias.parseJSON(JSON.stringify(data));
+		
 	var comments = null;
 	if(result.status == "success"){
+		
+		console.log("updateCommentsCallback result success");
+		
 		comments = result.content.comments;
 		var table = gootAlias('<table></table>').addClass('goot-' + scope + '-comments');
 		var l = comments.length;
 		if(l==0){
+			console.log("no comments found");
 			gootAlias("#goot-" + scope + "-no-comments").show();
 		}
 		for(var i = 0; i < l; i++){
 			var comment = comments[i];
 			var row = null;
+			console.log("comment : " + comment);
 			if(comment.type == 0){ //image
 				row = gootAlias('<tr></tr>').text("image comment with id " + comment.id);
 			}else if(comment.type == 1){ //text
